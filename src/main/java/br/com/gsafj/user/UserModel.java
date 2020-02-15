859f3b26db594e52e82b1e1786928e1cb788f50a
Gerson Sales <gerson.junior@ccc.ufcg.edu.br>
2020-02-15 14:57:35 -0300

    create initial user db model

--- src/main/java/br/com/gsafj/user/UserModel.java
+++ src/main/java/br/com/gsafj/user/UserModel.java
@@ -1,10 +1,28 @@
 package br.com.gsafj.user;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
+import javax.persistence.*;
 import java.io.Serializable;
 
-@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Entity
+@Table(name = "user")
 public class UserModel implements Serializable {
   private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Double id;
+
+  @Column(name = "first_name")
+  private String firstName;
+
+  @Column(name = "last_name")
+  private String lastName;
+
+  @Column(name = "password")
+  private String password;
+
+
+  public final Double getId() {
+    return id;
+  }
 }
