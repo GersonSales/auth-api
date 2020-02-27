5bd2e2222b90ddbe84db2f5995b2c77a40dd0643
Gerson Sales <gerson.junior@ccc.ufcg.edu.br>
2020-02-27 09:54:13 -0300

    create initial permission table

--- src/main/java/br/com/gsafj/user/Permission.java
+++ src/main/java/br/com/gsafj/user/Permission.java
@@ -1,5 +1,26 @@
 package br.com.gsafj.user;
 
 
-public class Permission {
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity
+public class Permission implements GrantedAuthority, Serializable {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private String id;
+
+  private String authority;
+
+
+  @Override
+  public String getAuthority() {
+    return this.authority;
+  }
 }
